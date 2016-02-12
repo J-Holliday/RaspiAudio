e97dbedab98200ad1309a7c5592ab74c124788f6
J-Holliday <14ec004@ms.dendai.ac.jp>
2016-02-12 10:06:31 +0900

    add cirno2.sh

---
+++ cirno2.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+~/workspace/raspi-audio/downloads/julius-4.3.1/julius/julius -C ~/workspace/raspi-audio/downloads/julius-kits/dictation-kit-v4.3.1-linux/am-gmm.jconf -C ~/workspace/raspi-audio/downloads/julius-kits/dictation-kit-v4.3.1-linux/main.jconf -input rawfile
